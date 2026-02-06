02bab2e53089234a0e46b2669cb883a01409930c
Yuliia Kolomiiets <jk182013@gmail.com>
2026-02-06 10:33:20 +0100

    init projet

---
+++ module.js
@@ -0,0 +1,13 @@
+/**
+ * Calculates the age of a person based on their birth date in years.
+ * 
+ * @param {object} p An object representing a person with a birthDate property of type Date 
+ * @returns {number} The age of the person in years
+ */
+
+
+function calculateAge(p) {
+  let dateDiff = new Date(Date.now()) - p.birthDate.getTime();
+  let age = Math.abs(dateDiff.getUTCFullYear() - 1970);
+  return age;
+}
